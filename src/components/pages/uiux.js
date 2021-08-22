6db0b07fa600d72db3c8284c31bc4893076c49b2
ytkuo <hahaeating1@gmail.com>
2021-08-22 13:07:02 -0400

    8/22 uiux component building

--- src/components/pages/uiux.js
+++ src/components/pages/uiux.js
@@ -1,57 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import uiuxDetail from '../uiuxDetails';
 import { Contact } from '../general.js';
 
 function SVGStar() {
-    let lineIndex = 0;
-    window.onscroll = function () {
-        const star = document.querySelectorAll(".star");
-        const spot = document.querySelectorAll(".star path");
-        const svg = document.querySelectorAll(".star svg");
-        const primaryLine = document.querySelectorAll(".star .primaryLine");
-        let sectionIndex = 0;
-        let sectionTop = star[0].getBoundingClientRect().top;
-        
-        star.forEach((item, idx) => {
-            // console.log(star[idx + 1].getBoundingClientRect().top);
-            if (sectionTop <= 0) {
-                sectionTop = item.getBoundingClientRect().top;
-                sectionIndex = idx;
-            }
-        })
-
-        if (sectionTop <= 330 && sectionTop >= -500) {
-            spot[sectionIndex].style.fill = "#AD8255";
-            svg[sectionIndex].classList.add("animation");
-        } else {
-            if (sectionIndex > 0) {
-                spot[sectionIndex - 1].style.fill = "#E5E5E5";
-                svg[sectionIndex - 1].classList.remove("animation");
-            }
-        }
-
-        if (star.length > lineIndex + 1) {
-            if (star[lineIndex + 1].getBoundingClientRect().top <= 0) {
-                lineIndex += 1;
-            }
-            primaryLine[lineIndex].style.height = `${Math.abs(330 - star[lineIndex].getBoundingClientRect().top)}px`;
-        }
-        // console.log(lineIndex);
-        // console.log(Math.abs(330 - star[lineIndex].getBoundingClientRect().top));
-        
-
-    };
-
     return (
         <>
             <div className="star">
-                <span className="greyLine">
-                    <svg width="24" height="29" viewBox="0 0 24 29" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 0.000782013C9.08258 0.00422383 6.28559 1.16471 4.22263 3.22767C2.15966 5.29063 0.999181 8.08762 0.995739 11.0051C0.992245 13.3892 1.77102 15.7087 3.21261 17.6077C3.21261 17.6077 3.51272 18.0028 3.56174 18.0598L12 28.0117L20.4423 18.0548C20.4864 18.0018 20.7875 17.6077 20.7875 17.6077L20.7885 17.6047C22.2293 15.7065 23.0078 13.3881 23.0043 11.0051C23.0009 8.08762 21.8404 5.29063 19.7775 3.22767C17.7145 1.16471 14.9175 0.00422383 12 0.000782013ZM15.6015 16.007L12 13.7151L8.39864 16.007L8.99887 11.9314L5.9977 9.17737L10.1993 8.67017L12 5.00274L13.9128 8.67117L18.0024 9.17737L15.0012 11.9314L15.6015 16.007Z" fill="#AD8255" />
-                    </svg>
-                </span>
-                <span className="d-block primaryLine"></span>
+                <svg width="24" height="29" viewBox="0 0 24 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M12 0.000782013C9.08258 0.00422383 6.28559 1.16471 4.22263 3.22767C2.15966 5.29063 0.999181 8.08762 0.995739 11.0051C0.992245 13.3892 1.77102 15.7087 3.21261 17.6077C3.21261 17.6077 3.51272 18.0028 3.56174 18.0598L12 28.0117L20.4423 18.0548C20.4864 18.0018 20.7875 17.6077 20.7875 17.6077L20.7885 17.6047C22.2293 15.7065 23.0078 13.3881 23.0043 11.0051C23.0009 8.08762 21.8404 5.29063 19.7775 3.22767C17.7145 1.16471 14.9175 0.00422383 12 0.000782013ZM15.6015 16.007L12 13.7151L8.39864 16.007L8.99887 11.9314L5.9977 9.17737L10.1993 8.67017L12 5.00274L13.9128 8.67117L18.0024 9.17737L15.0012 11.9314L15.6015 16.007Z" fill="#AD8255" />
+                </svg>
+                <span className="greyLine"></span>
+                <span className="primaryLine"></span>
             </div>
         </>
     )
@@ -112,6 +72,8 @@ function InformationArchitecture(props) {
 function Mockup(props) {
     const { content } = props;
 
+    // 用useState改背景顏色
+
     return (
         <>
            {content.map((item, key) => ( // can't use forEach, it can't display properly
@@ -132,7 +94,7 @@ function Mockup(props) {
                         <video className={item.frame !== undefined ? 'mobileVideo' : 'desktopVideo'} controls>
                             <source src={item.video} type="video/mp4" />
                         </video>
-                        {item.background !== false ? <div className="mockupBgc akBgc"></div> : ""} 
+                       {item.background !== false ? <div className="mockupBgc akBgc"></div> : ""} 
                     </div>
                     
                     <div className={item.img !== undefined ? 'd-flex justify-content-center imgSection' : 'd-none'}>
@@ -150,8 +112,54 @@ function Uiux() {
     const { title } = useParams();
     let stepCount = 1;
 
+    const handleScroll = () => {
+        const star = document.querySelectorAll(".star");
+        const spot = document.querySelectorAll(".star path");
+        const svg = document.querySelectorAll(".star svg");
+        const primaryLine = document.querySelectorAll(".star .primaryLine");
+        let sectionIndex = 0;
+        let sectionTop = star[0].getBoundingClientRect().top;
+        
+        star.forEach((item, idx, ary) => {
+            if (idx === 0) {
+                if (item.getBoundingClientRect().top <= 330) {
+                    primaryLine[idx].style.height = `${window.pageYOffset - 250}px`;
+                } else {
+                    primaryLine[idx].style.height = '0px';
+                }
+            } else if (ary.length > idx + 1) {
+                if (item.getBoundingClientRect().top <= 330) {
+                    primaryLine[idx].style.height = `${Math.abs(330 - item.getBoundingClientRect().top)}px`;
+                } else {
+                    primaryLine[idx].style.height = '0px';
+                }
+            }
+
+            if (sectionTop <= 0) {
+                sectionTop = item.getBoundingClientRect().top;
+                sectionIndex = idx;
+            }
+        })
+
+        if (sectionTop <= 330) {
+            spot[sectionIndex].style.fill = "#AD8255";
+            svg[sectionIndex].classList.add("animation");
+        } else {
+            if (sectionIndex > 0) {
+                svg[sectionIndex - 1].classList.remove("animation");
+            }
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
     return (
-        <main>
+        <main onScroll={handleScroll}>
             {/* use the result of filter function */}
             {uiuxDetail.filter(content => content.urlName === title).map((content, key) => (
                 <div key={key}>
--- src/components/uiuxDetails.js
+++ src/components/uiuxDetails.js
@@ -3,7 +3,6 @@ import ak2 from '../imgs/work/ak_step1-2.png';
 import ak3 from '../imgs/work/ak_step3.png';
 import ak4 from '../imgs/work/ak_step4.png';
 import ak5 from '../imgs/work/ak_step5-0.png';
-import ak6 from '../imgs/work/ak_step5-1.png';
 import ak7 from '../imgs/work/ak_step5-2.png';
 import ak8 from '../imgs/work/ak_step5-3.png';
 import ak9 from '../imgs/work/ak_step5-4.png';
@@ -22,11 +21,21 @@ import ak21 from '../imgs/work/ak_step5-16.png';
 import ak22 from '../imgs/work/ak_step5-17.png';
 import ak23 from '../imgs/work/ak_step5-18.png';
 import ak24 from '../imgs/work/ak_step5-19.png';
+import rb1 from '../imgs/work/rabbit_step1-1.png';
+import rb2 from '../imgs/work/rabbit_step1-2.png';
+import rb3 from '../imgs/work/rabbit_step3.png';
+import rb4 from '../imgs/work/rabbit_step4.png';
+import rb5 from '../imgs/work/rabbit_step5-1.png';
+import rb6 from '../imgs/work/rabbit_step5-2.png';
+import rb7 from '../imgs/work/rabbit_step5-3.png';
+import rb8 from '../imgs/work/rabbit_step5-4.png';
+import rb9 from '../imgs/work/rabbit_step5-5.png';
 import akVideo1 from '../imgs/work/ak_video1.mov';
 import akVideo2 from '../imgs/work/ak_video2.mov';
 
 const uiuxDetail = [
-    {
+    // AK_Jewelry
+    { 
         urlName: 'AK_Jewelry',
         mainTitle: 'Andrea Ko Jewelry Design (RWD)',
         description: '“Andrea Ko Jewelry Design Website” is mainly for recruiting students. However, the original online presence and the information were lacking. After I talked to Andrea, she told us most website visitors were first-time users, and they came to the website to search the information of jewelry beginner course. Thus, I redesigned the whole website (RWD) to make it more organized and clean, primarily focused on the user flow of the jewelry beginner course.',
@@ -47,7 +56,7 @@ const uiuxDetail = [
         ppAnalysis: [
             "Information is presented unclear and confusing.",
             "There is no hierarchy among the fonts. All of fonts are in same size and same weight.",
-            " Sentences need to be simplified. ",
+            "Sentences need to be simplified.",
         ],
         informationArchitecture: {
             paragraph: 'The architecture of the website is not complicated. The only thing that made me hesitate was putting “Portfolio” into the “About” section. Finally, I decided to keep them in different sections because Andrea Ko is also a freelancer. It would be more convenient for a company to see the portfolio directly.',
@@ -102,14 +111,17 @@ const uiuxDetail = [
             }
         ]
     },
+    // TPE_Rabbit_Association
     {
         urlName: 'TPE_Rabbit_Association',
         mainTitle: 'Taipei Rabbit Saving Association Website (RWD)',
         description: 'Taipei Rabbit Saving Association is a nonprofit organization that dedicates to save stray rabbits. However, they had a really complicated website. There was too much information without being well organized. Thus, I redesigned one of the vital services, the adopted process, to help them enhance the chance of adoption.',
         research: [
             {
+                inline: true,
                 title: 'Persona',
                 paragraph: 'The user is a 30 years old woman called Hui-Lin Kao. She works as a purchasing with a good salary. With a stable life, Hui-Lin is planning to adopt a rabbit. Her ideal one is with white fur and red eyes. Therefore, she needs to find an organization she can trust and get clear information about adoption from the website.',
+                img: rb1,
             },
             {
                 title: 'Usability testing',
@@ -118,9 +130,54 @@ const uiuxDetail = [
                     'Assess User Flow: Homepage - Click “see all rabbits” on the bottom of the page - Click in a rabbit introduction page - See the details and click “Adoption”- Read the rules before continue - Fill in a form from the outside link - Finish!',
                     'Task: You\'re considering adopting a rabbit. Please visit Taipei Saving Rabbit Association and see how to adopt a rabbit.',
                     'Result Of Testing: I analyze which pages participants went to, what participants were searching for on the page, what did they feel, what the status was while they were doing the task. Further, I listed the problems and difficulties and indicated which usability the original website was against. Lastly, I used effectiveness, efficiency, and satisfaction to analyze the priority of which page should be the first deal with.',
-                ]
+                ],
+                img: rb2,
             }
         ],
+        ppAnalysis: [
+            "There are too much information on the homepage.",
+            "Rabbits classification page is unclear. It’s hard finding the rabbit efficiently.",
+            "There are too many words and paragraphs on the rabbit description page. Everything seems crowded and makes users losing patience.",
+        ],
+        informationArchitecture: {
+            paragraph: 'I confirmed information architecture with the rabbit saving association through “Card Sorting .”During the closed sort, I founded that they wanted to put everything on the homepage. Commonly, clients feel everything is essential, so they want to put everything on the homepage. It is my responsibility to help them figure out the hierarchy of the website.',
+            img: rb3,
+        },
+        wireframe: rb4,
+        mockup: [
+            {
+                title: 'Overview',
+                paragraph: ['I use pink as the main color of website. Pink represents soft, gentle and cute impression to people. There is not much animation in this project. Everything is simple and intuitive.'],
+                video: akVideo2,
+            },
+            {
+                title: 'Homepage',
+                paragraph: ['I divided the homepage into three sections. First is the hero image with the slogan ”Hello! Could you please help me? ” and a donating button under the sentence. The second part is the carousel of a bulletin board. In the third part, users can see three recommended rabbits for adoption. They also can click the “see all the rabbits” button in this section.'],
+                img: [rb5],
+            },
+            {
+                title: 'Classification',
+                paragraph: [
+                    'Users can find all rabbits on the classification page and use a left-side filter to find the rabbit efficiently. The filters\' values for users to click are gender, adopted status, variety, fur\'s colour, and so on.',
+                    'On the result part, some vital information is listed under the image, such as name, adopted status, age, variety, and weight. If there is no data for the searching result, the system will recommend other rabbits with a sentence like "These rabbits are eager for a sweet home" to stimulate the users\' empathy.',
+                ],
+                img: [rb6, rb7],
+            },
+            {
+                title: 'Rabbit Introduction',
+                paragraph: ['On the individual rabbit introduction page, users can see basic information and the background story of the rabbit, which stimulate users\' empathy. Further, there are detailed health records for people to know the status of the rabbit. By offering information on the up and up, it will improve credibility for the user.'],
+                img: [rb8],
+            },
+            {
+                title: 'Must Know Before Adoption',
+                paragraph: ['When they click the “Adoption” button, users will come to the page with all the information they should know before they move on. This page eliminates those who aren’t qualified or aren’t well prepared to welcome a new family member. Moreover, there are common questions and answers for users to check before moving to the next step.'],
+                img: [rb9],
+            },
+            {
+                title: 'Adoption Form',
+                paragraph: ['As the association requires, the button will lead to a google form to apply for adoption. Therefore, the process of an adopted redesign of the website is finished.'],
+            },
+        ]
     }
 ]
 
